b8025383413bfaf87bc39bc574ba02e670f50be7
Adorax <jeremy.maret@gmail.com>
2023-01-18 23:34:50 +0100

    feat(comment): write code that explain by itself

--- pep8.py
+++ pep8.py
@@ -27,3 +27,52 @@ def is_leap_year(year):
 
 # Ainsi, un guide existe pour faire du code python maintenable : PEP 8
 # PEP pour Python Enhancement Proposal (proposition d'amélioration de Python).
+# https://peps.python.org/pep-0008/
+
+# variables en minuscule avec underscore
+fuel_level = "1"
+# courantes en majuscule avec underscore
+FIRST_DAY_WEEK = "Monday"
+# classe en CapitalizedCase
+class BigDog:
+    pass
+# nom de modules en minuscule en evitant les underscore
+import os
+import pathlib # contraction de “path” et “lib”
+
+# Commenter le code
+    # Mettez les commentaires à jour lorsque le code change.
+    # Évitez les commentaires sur la même ligne que le code, ils font négligé.
+    # Mettez un seul espace entre le symbole dièse et le texte (pour différencier du code qui a été commenté).
+    # Utilisez le même niveau d’indentation que la ligne de code qui suit, pour une bonne lisibilité
+
+# Utiliser le docstrings --> avec 3 quotes """ ... """
+    # Selon la PEP 257, il est bon d’utiliser les docstrings partout. (Surtout les classes publiques).
+    # commentaires spéciaux écrits au tout début d’une fonction, d’une classe ou d’un module, et utilisés pour la documentation
+    # Permets aux outils automatisés d’accéder au texte, par exemple pour générer une documentation en ligne de votre code.
+    # Notez que la docstring ne doit jamais décrire le fonctionnement interne de sa fonction ou méthode, mais seulement son retour. Il ne s’agit pas ici d’expliquer le code, mais d’expliquer son usage. C’est tout son intérêt.
+    # Decrire retour, paramètres et exceptions
+def multiply(first, second=1):
+    """Multiply two numbers.
+
+    Args:
+        first -- the first number
+        second -- the second one (default 1)
+    """
+    return first * second
+
+print(multiply(13, 77))
+# Les docstrings en début de fonction ou de classe sont spéciales, et on peut y accéder en utilisant l’attribut  __doc__  :
+print(multiply.__doc__)
+def search_film(name):
+    """Cherche un film selon un nom donné.
+
+    Attrs:
+    - name (str): le nom utilisé pour la recherche.
+
+    Returns:
+    - un objet `film` si un film a été trouvé, ou None.
+    """
+    # ...
+
+# En Python, rien n’est vraiment privé. Mais préfixer vos méthodes d’un underscore pour signaler qu’elles sont privées.
