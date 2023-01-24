10074f481c43c4d842aa62cfae462e8484b395de
Jérémy Maret <jeremy.maret@gmail.com>
2023-01-24 18:37:24 +0100

    Create environement.py

---
+++ environement.py
@@ -0,0 +1,7 @@
+# Setup env
+# creer environement env (env est le nom qu'on donne par convention mais peut-être modifié) : 'python -m venv env'
+# dans le repertoire : 'source env/bin/activate'
+# desactiver l'env : 'deactivate'
+
+# voir les modules de cette env : 'pip freeze'
+# voir info sur package : 'pip show requests'
