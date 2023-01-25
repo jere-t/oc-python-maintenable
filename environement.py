96e6ef121dd1618b1caca16155aac773a52f4742
Jérémy Maret <jeremy.maret@gmail.com>
2023-01-25 09:41:04 +0100

    Update environement.py

--- environement.py
+++ environement.py
@@ -1,4 +1,4 @@
-# Setup env
+# Setup env (https://openclassrooms.com/fr/courses/6951236-mettez-en-place-votre-environnement-python)
 # creer environement env (env est le nom qu'on donne par convention mais peut-être modifié) : 'python -m venv env'
 # dans le repertoire : 'source env/bin/activate'
 # desactiver l'env : 'deactivate'
