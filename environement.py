bb39a668fb4dcb87104b2ee8d3478de720cf147b
Jérémy Maret <jeremy.maret@gmail.com>
2023-01-25 09:33:06 +0100

    Update environement.py

--- environement.py
+++ environement.py
@@ -5,3 +5,10 @@
 
 # voir les modules de cette env : 'pip freeze'
 # voir info sur package : 'pip show requests'
+
+# Creer le fichier requirements.txt : 'pip freeze > requirements.txt'
+# Quand on arrive dans un nouveau projet : 'python -m venv env && source env/bin/activate && pip install -r requirements.txt'
+# 
+# Supprimer environement virtuel : 'rm -r env/'
+
+
